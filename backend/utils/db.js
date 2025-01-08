685c71abc027e4dbe6e31135ead9e7e51f8da630
AnuragShukla1999 <shuklaanurag310@gmail.com>
2025-01-08 22:12:53 +0530

    .

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
+    console.log("process.env.MONGO_URI ----> ", process.env.MONGO_URI)
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully.');
     } catch (error) {
-        console.log(error);
+        console.log("error----> ", error.message);
     }
 }
 export default connectDB;
---
+++ frontend/src/service/Api.jsx
@@ -0,0 +1,1 @@
+export default BACKEND_API = 'https://localhost:5000/api/v1'
